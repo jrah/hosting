bc62fba17eceb164cd9d14febb8cade3b5978948
Taerin Lee <taerin.lee90@gmail.com>
2020-06-03 10:28:54 +0900

    'fatal:change_content'

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,18 @@
 <template>
   <div class="container">
-    <div>
-      <Menu />
-      <h1 class="title">
-        hosting
-      </h1>
-      <h2 class="subtitle">
-        My well-made Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <Menu />
+    <div class="content">
+      <span>{{ title }}</span>
+      <h1>{{ bigTitle }}</h1>
+      <p>{{ description }}</p>
+      <table>
+        <tr>
+          <td v-for="type in types" :key="type">{{ type }}</td>
+        </tr>
+        <tr>
+          <td v-for="plan in plans" :key="plan">{{ plan }}</td>
+        </tr>
+      </table>
     </div>
   </div>
 </template>
@@ -30,44 +23,71 @@ import Menu from '~/components/Menu.vue'
 export default {
   components: {
     Menu
+  },
+  data() {
+    return {
+      title: 'Hosting Price',
+      bigTitle: '호스팅 비용 안내',
+      description:
+        'Vue119는 컨텐츠 관리 시스템(CMS)기반으로 Netlify를 사용하여 보다 편하고 저렴하게 홈페이지를 관리 할 수 있도록 도와드립니다.',
+      types: ['Prismic(CMS)', 'Netlify(Hosting)', 'JavaScript(Main Code)'],
+      plans: [
+        '무료',
+        '무료(게시글 등록, 팝업 수정시 배포함에 따라 작은 금액의 추가 요금 발생)',
+        '빠르고 안정적인 반응속도'
+      ]
+    }
   }
 }
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+<style scoped>
+/* @font-face {
+  font-family: NotoSansKR;
+  src: url('~/assets/fonts/NotoSansKR-Light.otf');
 }
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+@font-face {
+  font-family: Roboto;
+  src: url('~/assets/Roboto-Medium.ttf');
+} */
+
+.content {
+  margin-top: 30px;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  /* font-family: NotoSansKR; */
+}
+span {
+  /* font-family: Roboto; */
+  font-size: 14px;
+  color: #22a266;
+}
+h1 {
+  font-size: 35px;
+}
+table {
+  border: 0.5px solid #d3e5dc;
+  border-collapse: collapse;
+  width: 1020px;
+  margin-top: 20px;
   text-align: center;
 }
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+tr:first-child {
+  /* font-family: Roboto; */
+  background-color: #22a266;
+  color: white;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+tr:last-child {
+  font-weight: bold;
 }
-
-.links {
-  padding-top: 15px;
+tr:last-child td {
+  padding: 30px;
+}
+td {
+  width: 33%;
+  border: 0.5px solid #d3e5dc;
+  padding: 15px;
 }
 </style>
